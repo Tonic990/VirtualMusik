6804d5a582b93b6c1fe67aace25f22aacb1c72b5
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-03 22:01:46 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -36,7 +36,6 @@ def cb_admin_check(func: Callable) -> Callable:
         admemes = a.get(cb.message.chat.id)
         if cb.from_user.id in admemes:
             return await func(client, cb)
-        else:
             await cb.answer("you not allowed to do this!", show_alert=True)
             return
         
