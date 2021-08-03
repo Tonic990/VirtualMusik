c26accef0c3e01c51ee79376d7a8c92e355037a9
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-03 22:20:52 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -47,3 +47,11 @@ def authorized_users_only(func: Callable) -> Callable:
                 return await func(client, message)
 
     return decorator
+
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+    return decorator
