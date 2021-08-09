3fc6ea99a10ede09ee52e1d16b95d07df00a3ae7
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-09 20:29:54 +0700

    fixes

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -5,7 +5,8 @@ from helpers.decorators import authorized_users_only, errors
 from callsmusic.callsmusic import client as USER
 from config import SUDO_USERS
 
-@Client.on_message(filters.command(["veezjoin"]) & ~filters.private & ~filters.bot)
+
+@Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -14,53 +15,53 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>make me as admin first</b>",
+            "<b>Add me as admin of yor group first</b>",
         )
         return
 
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "music player"
+        user.first_name = "DaisyMusic"
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id, "🤖: i'm joined here to play music on voice chat.")
+        await USER.send_message(message.chat.id, "I joined here as you requested")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>assistant music already in your chat</b>",
+            "<b>helper already in your chat</b>",
         )
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n The {user.first_name} can't join to your group, the assistant has been banned by admin in this group."
-            "\n\nplease unbanned the assistant or add manually.</b>",
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
+            "\n\nOr manually add Asisstant to your Group and try again</b>",
         )
         return
     await message.reply_text(
-        "<b>assistant music joined your chat.</b>",
+        "<b>helper userbot joined your chat</b>",
     )
 
 
-@USER.on_message(filters.group & filters.command(["vleave"]))
+@USER.on_message(filters.group & filters.command(["userbotleave"]))
 @authorized_users_only
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:
         await message.reply_text(
-            f"<b>assistant can't leave chats because waiting for floodwait."
-            "\n\nor kick assistant manually</b>",
+            f"<b>User couldn't leave your group! May be floodwaits."
+            "\n\nOr manually kick me from to your Group</b>",
         )
         return
     
-@Client.on_message(filters.command(["vleaveall"]))
+@Client.on_message(filters.command(["userbotleaveall"]))
 async def bye(client, message):
     if message.from_user.id in SUDO_USERS:
         left=0
         failed=0
-        await message.reply("assistant leaving all chats !")
-        for dialog in USER.iter_dialogs():
+        lol = await message.reply("Assistant Leaving all chats")
+        async for dialog in USER.iter_dialogs():
             try:
                 await USER.leave_chat(dialog.chat.id)
                 left = left+1
