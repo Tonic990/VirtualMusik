f66d474216459ec5821829878f34c26c771a0c82
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-10 15:57:42 +0700

    setup

--- handlers/start.py
+++ handlers/start.py
@@ -132,7 +132,7 @@ async def help(client: Client, message: Message):
     )
 
 
-@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & filters.private & ~filters.edited)
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
 async def ping_pong(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text("pinging...")
