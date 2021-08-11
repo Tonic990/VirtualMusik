93defbe4f7b05c9f28b94773412fae9399a29237
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-12 01:34:04 +0700

    pushed

---
+++ handlers/insong.py
@@ -0,0 +1,19 @@
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+from callsmusic.callsmusic import client as veez
+
+
+@Client.on_message(command(["vk", f"vksong@{BOT_USERNAME}"]))
+async def songs(client, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("❗ **song not found.**\n\n**please give a valid song name.**")
+            return
+        text = message.text.split(None, 1)[1]
+        results = await veez.get_inline_bot_results(119606003, f"song {text}")
+        await veez.send_inline_bot_result(
+            message.chat.id, results.query_id, results.results[0].id
+        )
+    except Exception:
+        await message.reply_text("❗ **song not found.**")
