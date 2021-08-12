1e9ffbac5d88e030427228e01010c2662fc33382
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-12 19:38:01 +0000

    fix

--- handlers/insong.py
+++ handlers/insong.py
@@ -4,14 +4,14 @@ from helpers.filters import command
 from callsmusic.callsmusic import client as veez
 
 
-@Client.on_message(command(["vk", f"vksong@{BOT_USERNAME}"]))
+@Client.on_message(command(["vk", f"vk@{BOT_USERNAME}"]))
 async def songs(client, message):
     try:
         if len(message.command) < 2:
             await message.reply_text("❗ **song not found.**\n\n**please give a valid song name.**")
             return
         text = message.text.split(None, 1)[1]
-        results = await veez.get_inline_bot_results(119606003, f"song {text}")
+        results = await veez.get_inline_bot_results(1872165533, f"music {text}")
         await veez.send_inline_bot_result(
             message.chat.id, results.query_id, results.results[0].id
         )
