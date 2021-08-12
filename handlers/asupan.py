9595bfe6449354489ce511afdb76d34775328c25
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-12 19:46:10 +0000

    new module

---
+++ handlers/asupan.py
@@ -0,0 +1,37 @@
+# Module by https://github.com/tofikdn
+# Copyright (C) 2021 TdMusic
+# Ported by @levina-lab for VeezMusic
+
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
