5b4b7467b240da003c857b9e845748362979403d
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-12 20:03:10 +0000

    audio play function (alternative)

---
+++ handlers/audioplay.py
@@ -0,0 +1,71 @@
+# this module i created only for playing music using audio file, idk, because the audio player on play.py module not working so this is the alternative
+# audio play function
+
+from os import path
+
+from pyrogram import Client
+from pyrogram.types import Message, Voice
+
+from callsmusic import callsmusic, queues
+
+import converter
+from downloaders import youtube
+
+from config import BOT_NAME as bn, DURATION_LIMIT, UPDATES_CHANNEL
+from helpers.filters import command, other_filters
+from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@Client.on_message(command("stream") & other_filters)
+@errors
+async def stream(_, message: Message):
+
+    lel = await message.reply("🔁 **processing** sound...")
+    sender_id = message.from_user.id
+    sender_name = message.from_user.first_name
+
+    keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="📣 CHANNEL,
+                        url=f"https://t.me/{UPDATES_CHANNEL}")
+                   
+                ]
+            ]
+        )
+
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play!"
+            )
+
+        file_name = get_file_name(audio)
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name)) else file_name
+        )
+    elif url:
+        file_path = await converter.convert(youtube.download(url))
+    else:
+        return await lel.edit_text("❗ you did not give me anything to play!")
+
+    if message.chat.id in callsmusic.pytgcalls.active_calls:
+        position = await queues.put(message.chat.id, file=file_path)
+        await lel.edit(f"#⃣ **Queued** at position {position}!")
+    else:
+        callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+        await message.reply_photo(
+        photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
+        reply_markup=keyboard,
+        caption="🎧 **Playing** a song by {}!".format(
+        message.from_user.mention()
+        ),
+    )
+        return await lel.delete()
