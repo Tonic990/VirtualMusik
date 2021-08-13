93ab30acba4084be114dbf7645fbb20e9dea0a2d
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-13 14:21:27 +0000

    added more module

--- handlers/entertainment.py
+++ handlers/entertainment.py
@@ -35,3 +35,23 @@ async def chika(client, message):
         return await client.send_video(message.chat.id, video=results)
     except Exception:
         await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
+async def truth(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/truth").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
+
+
+@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+async def dare(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/dare").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
