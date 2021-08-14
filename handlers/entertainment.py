66d456d7075f16fa81400d6089578ac68df98355
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-14 06:21:06 +0000

    added lyrics scrapper

--- handlers/entertainment.py
+++ handlers/entertainment.py
@@ -55,3 +55,18 @@ async def dare(client, message):
         return await message.reply_text(results)
     except Exception:
         await message.reply_text("something went wrong...")
+
+
+@Client.on_message(command(["lyric", f"lyric@{bu}"]))
+async def lirik(_, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("**give a lyric name too !**")
+            return
+        query = message.text.split(None, 1)[1]
+        rep = await message.reply_text("🔎 **searching lyrics...**")
+        resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
+        result = f"{resp['data']}"
+        await rep.edit(result)
+    except Exception:
+        await rep.edit("**Lyrics not found.** please give a valid song name !")
