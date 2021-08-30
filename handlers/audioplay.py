30b82b72c1e46ec58e4ff807db56174393bd8743
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-30 21:55:20 +0700

    .

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -18,13 +18,13 @@ from helpers.decorators import errors
 from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from strings import string as _
+from strings import string as tgl
 
 @Client.on_message(command("stream") & other_filters)
 @errors
-async def stream(client, message):
+async def stream(_, client, message):
 
-    lel = await message.reply_text(_("process_one"))
+    lel = await message.reply_text(tgl("process_one"))
     sender_id = message.from_user.id
     sender_name = message.from_user.first_name
 
@@ -58,7 +58,7 @@ async def stream(client, message):
     elif url:
         file_path = await converter.convert(youtube.download(url))
     else:
-        return await lel.edit_text(_("where_file"))
+        return await lel.edit_text(tgl("where_file"))
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(message.chat.id, file=file_path)
