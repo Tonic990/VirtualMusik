a6ce589b6e6e0293ca908f1a217b20224086bf71
zxce3 <memetpea25@gmail.com>
2021-08-30 15:07:22 +0700

    fix import
        from strings import get_string as _audio
    ImportError: cannot import name 'get_string' from 'strings' (unknown location)

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -18,13 +18,13 @@ from helpers.decorators import errors
 from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from strings import get_string as _
+from strings import string as _lang
 
 @Client.on_message(command("stream") & other_filters)
 @errors
-async def stream(_, message: Message):
+async def stream(_lang, message: Message):
 
-    lel = await message.reply_text(_("process_one"))
+    lel = await message.reply_text(_lang("process_one"))
     sender_id = message.from_user.id
     sender_name = message.from_user.first_name
 
@@ -58,7 +58,7 @@ async def stream(_, message: Message):
     elif url:
         file_path = await converter.convert(youtube.download(url))
     else:
-        return await lel.edit_text(_("where_file"))
+        return await lel.edit_text(_lang("where_file"))
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(message.chat.id, file=file_path)
