9f768152703ba2748acdf82d370d484e0af51686
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-01 12:54:31 +0700

    new
    function to remove downloaded files

---
+++ handlers/rm_dl.py
@@ -0,0 +1,23 @@
+# function to remove the downloaded files
+
+import os
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from helpers.filters import command
+from helpers.decorators import sudo_users_only, errors
+
+downloads = os.path.realpath("downloads")
+
+
+@Client.on_message(command(["rmd", "rmdownloads", "cleardownloads"]) & ~filters.edited)
+@sudo_users_only
+@errors
+def clear_downloads(_, message: Message):
+    ls_dir = os.listdir(downloads)
+    if ls_dir:
+        for file in os.listdir(downloads):
+            os.remove(os.path.join(downloads, file))
+        message.reply_text("<b>✅ deleted all downloaded files</b>", quote=False)
+    else:
+        message.reply_text("<b>❌ no files downloaded</b>", quote=False)
