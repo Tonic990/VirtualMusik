3a5c7440226c92af9a3df8aa0116c0a776a5ab28
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-01 19:30:33 +0700

    Update rm_dl.py

--- handlers/rm_dl.py
+++ handlers/rm_dl.py
@@ -1,7 +1,6 @@
 # function to remove the downloaded files
 
 import os
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from helpers.filters import command
@@ -9,15 +8,14 @@ from helpers.decorators import sudo_users_only, errors
 
 downloads = os.path.realpath("downloads")
 
-
 @Client.on_message(command(["rmd", "rmdownloads", "cleardownloads"]) & ~filters.edited)
-@sudo_users_only
 @errors
+@sudo_users_only
 async def clear_downloads(_, message: Message):
     ls_dir = os.listdir(downloads)
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("<b>✅ deleted all downloaded files</b>", quote=False)
+        await message.reply_text("✅ **deleted all downloaded files**")
     else:
-        await message.reply_text("<b>❌ no files downloaded</b>", quote=False)
+        await message.reply_text("❌ **no files downloaded**")
