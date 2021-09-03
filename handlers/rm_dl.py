3dfec3a606a39844d9206b8da050423b4742c7e6
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-09-03 23:47:38 +0530

    Deletation of Raw Files

--- handlers/rm_dl.py
+++ handlers/rm_dl.py
@@ -7,6 +7,7 @@ from helpers.filters import command
 from helpers.decorators import sudo_users_only, errors
 
 downloads = os.path.realpath("downloads")
+raw = os.path.realpath("raw_files")
 
 @Client.on_message(command(["rmd", "rmdownloads", "cleardownloads"]) & ~filters.edited)
 @errors
@@ -16,6 +17,19 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **deleted all downloaded files**")
+        await message.reply_text("✅ **Deleted all downloaded files**")
     else:
-        await message.reply_text("❌ **no files downloaded**")
+        await message.reply_text("❌ **No files downloaded**")
+        
+@Client.on_message(command(["clean", "wipe", "rmr"]) & ~filters.edited)
+@errors
+@sudo_users_only
+async def clear_raw(_, message: Message):
+    ls_dir = os.listdir(raw)
+    if ls_dir:
+        for file in os.listdir(raw):
+            os.remove(os.path.join(raw, file))
+        await message.reply_text("✅ **Deleted all raw files**")
+    else:
+        await message.reply_text("❌ **No raw files**")
+        
