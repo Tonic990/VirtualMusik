372fad543fa5fe650c3474884c15f6e8d724c1e6
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-15 23:02:44 +0700

    fixes

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -58,3 +58,17 @@ def sudo_users_only(func: Callable) -> Callable:
             return await func(client, message)
 
     return decorator
+
+
+# Utils Helper
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
