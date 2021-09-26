aa8bd8e29d76b7842af023ce84b55dffcef0ccab
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-26 18:54:19 +0700

    good

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -15,19 +15,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **searching**")
+        m = await message.reply_text("🔎 **Searching**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"**Judul:** `{results[i]['title']}`\n"
-            text += f"**Durasi:** {results[i]['duration']}\n"
-            text += f"**Views:** {results[i]['views']}\n"
-            text += f"**Channel:** {results[i]['channel']}\n"
-            text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"🏷 **Name:** __{results[i]['title']}__\n"
+            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
+            text += f"👀 **Views:** `{results[i]['views']}`\n"
+            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
