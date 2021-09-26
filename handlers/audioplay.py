5edd029007f19c9c9b1873f9e817483bcb175d58
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-26 08:38:19 +0700

    Update audioplay.py
    ciee

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -1,16 +1,13 @@
 # this module i created only for playing music using audio file, idk, because the audio player on play.py module not working
 # so this is the alternative
 # audio play function
-
-from os import path
-
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
-from callsmusic import callsmusic, queues
+# tede ganteng tq
 
 import converter
-from downloaders import youtube
+from os import path
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from callsmusic import callsmusic, queues
 
 from config import (
     DURATION_LIMIT,
@@ -22,7 +19,7 @@ from config import (
 )
 from handlers.play import convert_seconds
 from helpers.filters import command, other_filters
-from helpers.gets import get_url, get_file_name
+from helpers.gets import get_file_name
 
 
 @Client.on_message(command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
@@ -45,24 +42,21 @@ async def stream(_, message: Message):
         )
 
     audio = message.reply_to_message.audio if message.reply_to_message else None
-    url = get_url(message)
 
-    if audio:
-        if round(audio.duration / 60) > DURATION_LIMIT:
-            return await lel.edit(f"❌ **music with duration more than** `{DURATION_LIMIT}` **minutes, can't play !**")
-
-        file_name = get_file_name(audio)
-        title = audio.title
-        duration = convert_seconds(audio.duration)
-        file_path = await converter.convert(
-            (await message.reply_to_message.download(file_name))
-            if not path.isfile(path.join("downloads", file_name)) else file_name
-        )
-    elif url:
+    if not audio:
         return await lel.edit("💭 **please reply to a telegram audio file**")
-    else:
-        return await lel.edit("💭 **please reply to a telegram audio file**")
+    if round(audio.duration / 60) > DURATION_LIMIT:
+        return await lel.edit(f"❌ **music with duration more than** `{DURATION_LIMIT}` **minutes, can't play !**")
 
+    # tede_ganteng = True
+    file_name = get_file_name(audio)
+    title = audio.title
+    duration = convert_seconds(audio.duration)
+    file_path = await converter.convert(
+        (await message.reply_to_message.download(file_name))
+        if not path.isfile(path.join("downloads", file_name)) else file_name
+    )
+    # ambil aja bg
     if message.chat.id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(message.chat.id, file=file_path)
         await message.reply_photo(
@@ -70,7 +64,6 @@ async def stream(_, message: Message):
             caption=f"💡 **Track added to queue »** `{position}`\n\n🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {costumer}",
             reply_markup=keyboard,
         )
-        return await lel.delete()
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
@@ -79,4 +72,5 @@ async def stream(_, message: Message):
                    +f"🎧 **Request by:** {costumer}",
             reply_markup=keyboard,
         )
-        return await lel.delete()
+
+    return await lel.delete()
