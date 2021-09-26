28fc2b9ddec0aab2efcfa7a6bf2afcdaba95c3b7
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-26 08:56:11 +0700

    Update admin_check.py
    oh boleh

--- helpers/helper_functions/admin_check.py
+++ helpers/helper_functions/admin_check.py
@@ -19,15 +19,12 @@ async def admin_check(message: Message) -> bool:
     user_id = message.from_user.id
 
     check_status = await client.get_chat_member(
-        chat_id=chat_id,
-        user_id=user_id
+        chat_id = chat_id,
+        user_id = user_id
     )
     admin_strings = [
         "creator",
         "administrator"
     ]
-    
-    if check_status.status not in admin_strings:
-        return False
-    else:
-        return True
+
+    return check_status.status in admin_strings
