dbf5dd005849ab56031a49918de967651fd164e2
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-10-02 04:47:45 +0700

    Update youtube.py
    gak

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -19,12 +19,7 @@ def download(url: str) -> str:
     duration = round(info["duration"] / 60)
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
-        )
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
