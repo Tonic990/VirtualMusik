d0f6bc91ddace009c2da788e52635474470f06e1
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-10-08 06:14:33 +0700

    Update audioplay.py

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -5,9 +5,6 @@
 
 from os import path
 
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 import converter
 from callsmusic import callsmusic, queues
 from config import (
@@ -21,13 +18,14 @@ from config import (
 from handlers.play import convert_seconds
 from helpers.filters import command, other_filters
 from helpers.gets import get_file_name
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
 @Client.on_message(command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
 async def stream(_, message: Message):
-
-    lel = await message.reply("🔁 **processing** sound...")
     costumer = message.from_user.mention
+    lel = await message.reply_text("🔁 **processing** sound...")
 
     keyboard = InlineKeyboardMarkup(
         [
@@ -43,7 +41,6 @@ async def stream(_, message: Message):
     )
 
     audio = message.reply_to_message.audio if message.reply_to_message else None
-
     if not audio:
         return await lel.edit("💭 **please reply to a telegram audio file**")
     if round(audio.duration / 60) > DURATION_LIMIT:
@@ -52,8 +49,8 @@ async def stream(_, message: Message):
         )
 
     # tede_ganteng = True
-    file_name = get_file_name(audio)
     title = audio.title
+    file_name = get_file_name(audio)
     duration = convert_seconds(audio.duration)
     file_path = await converter.convert(
         (await message.reply_to_message.download(file_name))
@@ -72,7 +69,8 @@ async def stream(_, message: Message):
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
             photo=f"{AUD_IMG}",
-            caption=f"🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n💡 **Status:** `Playing`\n🎧 **Request by:** {costumer}",
+            caption=f"🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n💡 **Status:** `Playing`\n"
+            + f"🎧 **Request by:** {costumer}",
             reply_markup=keyboard,
         )
 
