5369cb5e241da8c4eaadc4aafb2490f7bbd7f0b6
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-09 09:06:13 +0700

    log info

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pyrogram import Client as Bot
 
 from callsmusic import run
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from handlers import __version__
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
@@ -17,7 +18,7 @@ bot = Bot(
     plugins=dict(root="handlers"),
 )
 
-print("[INFO]: VEEZ MUSIC-PROJECT STARTED !")
+print(f"[INFO]: VEEZ MUSIC v{__version__} STARTED !")
 
 bot.start()
 run()
