23bcac7cbc9b771b91829011b6451fc83a3f742c
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-14 01:26:15 +0700

    try to change in latest py-tgcalls

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream
 
 from config import API_HASH, API_ID, SESSION_NAME
 
@@ -10,13 +12,19 @@ pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-def on_stream_end(chat_id: int) -> None:
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+    chat_id = update.chat_id
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        pytgcalls.leave_group_call(chat_id)
+        await pytgcalls.leave_group_call(chat_id)
     else:
-        pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
+        await pytgcalls.change_stream(
+            chat_id, 
+            InputAudioStream(
+                queues.get(chat_id)["file"],
+            ),
+        )
 
 
-run = pytgcalls.run
+run = pytgcalls.start
