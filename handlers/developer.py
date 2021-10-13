976d30eeaea5861a892653e8910fd05e767bf3e7
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-14 03:30:09 +0700

    done

--- handlers/developer.py
+++ handlers/developer.py
@@ -15,6 +15,14 @@ from helpers.decorators import sudo_users_only
 from config import BOT_USERNAME
 
 
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
 async def edit_or_reply(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
