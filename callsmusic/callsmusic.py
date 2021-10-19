c187e05b5360fc2eba630811fff7455556096281
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-19 16:37:48 +0700

    Revert "py-tgcalls update"

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,7 +1,5 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream
 
 from config import API_HASH, API_ID, SESSION_NAME
 
@@ -12,19 +10,13 @@ pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
-    chat_id = update.chat_id
+def on_stream_end(chat_id: int) -> None:
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
+        pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream(
-            chat_id, 
-            InputAudioStream(
-                queues.get(chat_id)["file"],
-            ),
-        )
+        pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
 
 
-run = pytgcalls.start
+run = pytgcalls.run
