51d00fbb76f4e7f68f91d207de048569c1b3673d
Shohih Abdul <50512936+DoellBarr@users.noreply.github.com>
2021-10-20 07:37:22 +0700

    Update play.py
    `mention` must callable, source [here](https://docs.pyrogram.org/api/types/User#pyrogram.types.User)

--- handlers/play.py
+++ handlers/play.py
@@ -842,7 +842,7 @@ async def lol_cb(b, cb):
         await b.send_photo(
             chat_id,
             photo="final.png",
-            caption=f"💡 **Track added to queue »** `{position}`\n\n🏷 **Name:** [{title[:35]}...]({url})\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {r_by.mention}",
+            caption=f"💡 **Track added to queue »** `{position}`\n\n🏷 **Name:** [{title[:35]}...]({url})\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {r_by.mention()}",
             reply_markup=keyboard,
         )
     else:
@@ -862,7 +862,7 @@ async def lol_cb(b, cb):
             chat_id,
             photo="final.png",
             caption=f"🏷 **Name:** [{title[:65]}]({url})\n⏱ **Duration:** `{duration}`\n💡 **Status:** `Playing`\n"
-            + f"🎧 **Request by:** {r_by.mention}",
+            + f"🎧 **Request by:** {r_by.mention()}",
             reply_markup=keyboard,
         )
     if path.exists("final.png"):
