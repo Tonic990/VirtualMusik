e7b0ab679ba1da20131960c8a1b91b484b87d490
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-10-22 02:34:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = Bot(
     plugins=dict(root="handlers"),
 )
 
-print(f"[INFO]: VEEZ MUSIC v{__version__} STARTED !")
+print(f"[INFO]: VIRTUAL MUSIC v{__version__} STARTED !")
 
 bot.start()
 run()
