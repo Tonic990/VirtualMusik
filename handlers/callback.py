0fdb1c2e3796fdfd3b599b54413019e7c09f83cd
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-10-22 02:20:20 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -46,7 +46,7 @@ async def cbstart(_, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 Source Code", url="https://github.com/levina-lab/VeezMusic"
+                        "🌐 Source Code", url="https://github.com/Tonic990/VirtualMusik"
                     )
                 ],
             ]
