0b0a7622e263e0df349596fdadac06f96ef9165d
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-03 12:13:54 +0700

    Update rmtrash.py

--- handlers/rmtrash.py
+++ handlers/rmtrash.py
@@ -9,8 +9,9 @@ from config import BOT_USERNAME
 from helpers.decorators import errors, sudo_users_only
 from helpers.filters import command
 
+
 downloads = os.path.realpath("downloads")
-raw = os.path.realpath("raw_files")
+raw = os.path.realpath("raw_files") # the code is not created for removing raw_files but if you want to create it, use this
 
 
 @Client.on_message(command(["rmd", "clean", f"rmd@{BOT_USERNAME}", f"clean@{BOT_USERNAME}"]) & ~filters.edited)
@@ -24,3 +25,17 @@ async def clear_downloads(_, message: Message):
         await message.reply_text("✅ **removed all downloaded files**")
     else:
         await message.reply_text("❌ **no files is downloaded**")
+
+
+@Client.on_message(command(["clear", f"clear@{BOT_USERNAME}"]) & ~filters.edited)
+@errors
+@sudo_users_only
+async def clear_jpg_image(_, message: Message):
+    pth = os.path.realpath(".")
+    ls_dir = os.listdir(pth)
+    if ls_dir:
+        for dta in os.listdir(pth):
+            os.system("rm -rf *.jpg")
+        await message.reply_text("✅ **succesfully cleared**")
+    else:
+        await message.reply_text("✅ **already cleared**")
